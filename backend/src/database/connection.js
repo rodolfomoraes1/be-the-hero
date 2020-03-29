af0d744e456a4ecf893e0a134993e980a355a8a3
Rodolfo Martins <rodolfomoraes1@gmail.com>
2020-03-29 12:22:22 +0100

    unit and integration tests configuration and startup

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,11 @@
 const knex = require("knex");
 const configuration = require("../../knexfile");
 
-const connection = knex(configuration.development);
+const dbEnviroment =
+  process.env.NODE_ENV === "test"
+    ? configuration.test
+    : configuration.development;
+
+const connection = knex(dbEnviroment);
 
 module.exports = connection;
